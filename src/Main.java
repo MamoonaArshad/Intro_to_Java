7a1d7c95cc51a9cfc1621f6d6ed055be4983a902
Mamoona Arshad <mamoona_arshad@hotmail.com>
2023-03-15 18:08:14 +0000

    java sentence

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ public class Main {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'k';
+        //char studentFirstInitial = 'k';
         char studentLatInitial = 'H';
         boolean hasPerfectAttendance = true;
+        String studentFirstName = "Kayla ";
+        String studentLastName = "Hammon";
+        char studentFirstInitial = studentFirstName.charAt(0);
 
-
-        System.out.println(studentAge);
+        System.out.println(studentFirstName + "" + studentLastName +  " has a GPA of" + studentGPA);
     }
 }
