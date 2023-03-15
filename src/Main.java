479ecb1eee75924885322625a6241ee341968077
Mamoona Arshad <mamoona_arshad@hotmail.com>
2023-03-15 22:10:55 +0000

    if loop with the pizza

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        scanner Scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
         boolean isOnRepeat = true;
 
-
+        while (isOnRepeat) {
+            System.out.println("Hunger level rising");
+            System.out.println("Would you like pizza?");
+            // not needed as variable defined in scope scanner Scanner = new Scanner(System.in);
+            String userInput = scanner.next();
+            if (userInput.equals("Yes")) {
+                isOnRepeat = false;
+                System.out.println("Coming now");
+            }
+            }
+        }
 
     }
-}
 
 
 
