3c0d7a9b72f53a250d2fd9947abdfd5303e86a8c
Mamoona Arshad <mamoona_arshad@hotmail.com>
2023-03-15 19:39:46 +0000

    starting

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Pick a number between 1 and 10");
-        Scanner scanner = new Scanner(System.in);
-        String favouriteFood = "pizza";
-        int inputtedNum = scanner.nextInt();
-        if (inputtedNum < 5) {
-             favouriteFood = "Chicken";
-            System.out.println("then your favourite food is " + favouriteFood);
-        }
-        else {
-            favouriteFood = "Ketchup chips";
-            System.out.println("then your favourite food is " + favouriteFood));
-        }
+        scanner Scanner = new Scanner (System.in);
+        boolean isOnRepeat = true;
+
 
 
     }
 }
+
+
+
+
+
