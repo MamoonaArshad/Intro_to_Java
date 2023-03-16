23ade962643e3a7e706409ab19a3866d10ed87b6
Mamoona Arshad <mamoona_arshad@hotmail.com>
2023-03-16 15:18:54 +0000

    Husband challenge completed

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,42 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean isOnRepeat = true;
-
-        while (isOnRepeat) {
-            System.out.println("Current song playing");
-            System.out.println("Would you like to play next song");
-            // not needed as variable defined in scope scanner Scanner = new Scanner(System.in);
-            String userInput = scanner.next();
-            if (userInput.equals("Yes")) {
-                isOnRepeat = false;
-                System.out.println("Next song is playing");
-            }
-            }
+        String question = "When is Wife's Birthday?";
+        String answer1 = "1: 3 September";
+        String answer2 = "2: 4 September";
+        String answer3 = "3: 9 September";
+        System.out.println(question);
+        System.out.println(answer1);
+        System.out.println(answer2);
+        System.out.println(answer3);
+        Scanner answer = new Scanner (System.in);
+        System.out.println("Write here the answer: " );
+        String userInput = answer.next();
+        if(userInput.equals("1")){
+            System.out.println("Bravo!");
         }
+        else{
+            System.out.println("Cafone!");
 
-    }
+        }
+            }
+
+        };
 
 
 
 
 
+
+
+
+
+
+
+
+//Implement a multiple choice question:
+//One question, three answer choices where one is correct
+//Ask the user a question and provide the question choices to user
+//the user will respond and we will collect their answer
+//print out the appropriate message to the user
