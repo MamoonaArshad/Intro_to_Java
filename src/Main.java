4c80c30e5337ef4acc1cbfd99e7c7627a32ace75
Mamoona Arshad <mamoona_arshad@hotmail.com>
2023-03-18 15:50:50 +0000

    Vacation days added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
-    public static double salaryInput(double numberOfHours, double raterPerHour){
-        double grossSalary = numberOfHours * raterPerHour;
+    public static double salaryInput(double totalNumberOfHours, double raterPerHour, double vacationHours){
+        double grossSalary = (totalNumberOfHours - vacationHours) * raterPerHour;
         return grossSalary;
 
 
     };
 
     public static void main(String[] args) {
-        double finalSalary = salaryInput(103, 25);
+        double finalSalary = salaryInput(1103, 25, 8 * 25);
         System.out.println(finalSalary);
 
 
