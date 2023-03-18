aa7be1702a801057db927dcd937ed633d4001a5a
Mamoona Arshad <mamoona_arshad@hotmail.com>
2023-03-18 15:47:15 +0000

    Salary calculator

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,22 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
 public class Main {
-    public static double fullMeal(double priceOfTheMeal, double taxPercentage, double tipPercentage) {
-        //here we declare the variables we are going to be using.
-        //The variable must always be declared in the round paranthesis as a list
+    public static double salaryInput(double numberOfHours, double raterPerHour){
+        double grossSalary = numberOfHours * raterPerHour;
+        return grossSalary;
 
-        double tipPrice = tipPercentage * priceOfTheMeal;
-        double taxPrice = taxPercentage * priceOfTheMeal;
-        double totalPrice = priceOfTheMeal  + tipPrice + taxPrice;
-        return totalPrice; //this will allow us to access the variable of the function for a different scope, it thus become kind of as an independent variable
-    }
 
-    public static void main(String[] args){
-       double groupTotalMealPrice = fullMeal(36, .20, .8);
-       //add directly the number in order
-        int numberOfPeople = 6;
-        double individualMealPrice = groupTotalMealPrice/numberOfPeople;
-        System.out.println(individualMealPrice);
+    };
+
+    public static void main(String[] args) {
+        double finalSalary = salaryInput(103, 25);
+        System.out.println(finalSalary);
 
 
 
     }
 };
+
+
+
 
 
 
